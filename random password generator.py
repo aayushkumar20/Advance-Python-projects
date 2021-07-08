751e13f1e8ac2aeac56b37ac8bbe59379aa1819a
aayushkumar20 <37871733+aayushkumar20@users.noreply.github.com>
2021-07-08 13:29:13 +0530

    created Random password generator

---
+++ random password generator.py
@@ -0,0 +1,11 @@
+import random
+
+lower="abcdefghijklmnopqrstuvwxyz"
+upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers="0123456789"
+symbols="@#$%^!~<>:;'/?"
+all=lower+upper+numbers+symbols
+#print("enter length of required password")
+length=20
+password="".join(random.sample(all,length))
+print(password)
