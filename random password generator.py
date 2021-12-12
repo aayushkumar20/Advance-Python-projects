d8bc7aa0ef6fd7f0d9e5d32fb403794d6f3f1edc
Aayush <37871733+aayushkumar20@users.noreply.github.com>
2021-12-12 10:24:32 +0530

    Update random password generator.py

--- random password generator.py
+++ random password generator.py
@@ -7,5 +7,5 @@ symbols="@#$%^!~<>:;'/?"
 all=lower+upper+numbers+symbols
 #print("enter length of required password")
 length=20
-password="".join(random.sample(all,length))
-print(password)
+output="".join(random.sample(all,length))
+print("password",output)
