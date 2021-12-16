68e7e66d54a54247f72b9769aa6369a62b6ee032
Aayush <37871733+aayushkumar20@users.noreply.github.com>
2021-12-16 08:47:33 +0530

    Add files via upload

---
+++ sketching/realtime sketcher.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+def sketch(image):
+    img_gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    img_gray_blur=cv2.GaussianBlur(img_gray,(6,6),0)
+    canny_edge=cv2.Canny(img_gray_blur,10,70)
+    ret,mask=cv2.threshold(canny_edge,70,255,cv2.THRESH_BINARY)
+    return mask
+
+cap=cv2.VideoCapture(0)
+while True:
+    ret,frame=cap.read()
+    cv2.imshow('sketcher',sketch(frame))
+    if cv2.waitKey(1)==13:
+        break
+cap.release()
+cv2.destroyAllWindows()
